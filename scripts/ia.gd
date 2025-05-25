aa7373864da91ab693b6bcc1a373c30037fed3de
EmilyM <emilymendes26@hotmail.com>
2025-05-25 14:32:34 -0300

    IA programada

--- scripts/ia.gd
+++ scripts/ia.gd
@@ -1,47 +1,47 @@
 extends Node2D
-@onready var player: CharacterBody2D = $"../player"
 @onready var timer: Timer = $Timer
 
-const altura_mapa = 4
+var player
 const largura_mapa = 8
 const limite_mapa = (largura_mapa * 16)/2
-const topo = -(16*altura_mapa)
 var heuristica_circulo = 100
 var heuristica_triangulo = 50
 var heuristica_quadrado = 0
 var formas
 var player_forma
 
-##ARRUMAR O JEITO QUE A HEURISTICA ESTA SENDO PASSADA DINAMICAMENTE
-
 func _ready() -> void:
 	timer.start()
 	randomize()
-	formas = define_heuristica('circulo')
-	print(formas)
-	formas = define_heuristica('circulo')
-	print(formas)
-	formas = define_heuristica('circulo')
-	print(formas)
 
 #func _physics_process(delta):
 
+func gerar_posicoes_x(largura: int, tamanho_tile: int) -> Array:
+	var limite = (largura * tamanho_tile) / 2
+	var posicoes = []
+	
+	for i in range(largura):
+		var x = -limite + (i * tamanho_tile) + tamanho_tile / 2
+		posicoes.append(x)
+		
+	return posicoes
 
 func spawn_inimigo(forma_escolhida):	
 	var inimigo = formas[forma_escolhida]["arquivo"].instantiate()
 	var eixo_x
-	if (player.position.x + 16 <= limite_mapa):
-		eixo_x = player.position.x + 16
-	else:
-		eixo_x = player.position.x - 16
+	var topo
+	var posicao_x
+	topo = player.position.y * (-1)
+	posicao_x = gerar_posicoes_x(largura_mapa,16)
+	eixo_x = posicao_x[randi() % posicao_x.size()]
 		
 	inimigo.position = Vector2(eixo_x, topo)
 	add_child(inimigo)
 	
 func define_heuristica(forma_player)-> Dictionary:
-	var heuristica_circulo
-	var heuristica_triangulo
-	var heuristica_quadrado
+	var heuristica_circulo = 0
+	var heuristica_triangulo = 0
+	var heuristica_quadrado = 0
 	
 	var numero = randi_range(50, 100)
 	var numero2 = randi_range(50,100)
@@ -79,8 +79,54 @@ func define_heuristica(forma_player)-> Dictionary:
 	return formas
 
 func algoritmo_guloso(formas):
-	var melhor_forma
-	var maior_custo
+	var forma_escolhida
+	var maior_custo = 0
+	var custo = 0
+	
+	for nome_forma in formas.keys():
+		custo = formas[nome_forma]["custo"]
+		
+		if custo > maior_custo:
+			maior_custo = max(custo, maior_custo)
+			forma_escolhida = nome_forma
+			
+	if maior_custo != 0:
+		return forma_escolhida
+	
+func get_player():
+	var players = get_tree().get_nodes_in_group("player")
+	if players.size() > 0:
+		return players[0]
+	return null
 
 func _on_timer_timeout() -> void:
-	spawn_inimigo('circulo')
+	player = get_player()
+	if player == null:
+		return
+	else:
+		player_forma = player.forma 
+		
+	var forma_escolhida
+	formas = define_heuristica(player_forma)
+	forma_escolhida = algoritmo_guloso(formas)
+	spawn_inimigo(forma_escolhida)
+
+	
+
+#Ja defini heuristicas dinamicamente
+#Ja defini spawn dinamico -> avaliar se tem como melhorar a posição do spawn
+#definir o algoritmo guloso para escolher a melhor forma
+
+#Lógica e montagem:
+#Lógica:
+	#Mapeia forma do player quando for spawnar o inimigo OK
+	#Mando para o algoritmo definir a heurística OK
+	#Manda o dicionário para o algoritmo guloso escolher qual inimigo spawnar
+	#Manda a forma escolhida para o spawn que vai ser chamado no timeout OK
+	
+#Montagem:
+	#Só preciso que função da Heurística seja chamada no timeout!!
+	#1 - timeout : forma_player
+	#2 - timeout : define heuristica
+	#3 - timeout : algoritmo guloso
+	#4 - timeout: spawn inimigo
--- scripts/tiro.gd
+++ scripts/tiro.gd
@@ -3,7 +3,7 @@ extends Area2D
 const altura_mapa = 4
 const limite_mapa = (16 * altura_mapa)/2
 var velocidade = 16
-var tempo_tiro = 0.5
+var tempo_tiro = 0.2
 var tempo = 0
 
 func _physics_process(delta):
